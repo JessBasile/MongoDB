c1095dbc02cd4eaac6d4dfc89ea3c9cb1bbd457b
Jesica Basile <jessbrenba@gmail.com>
2024-09-17 15:57:40 -0300

    mongo_wait.sh

--- mongo_wait.sh
+++ mongo_wait.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
 echo "Waiting for MongoDB to be ready..."
-until docker exec -it mongodb mongo --eval "print(\"waited for connection\")"
+until docker exec mongodb mongo --eval "print('waited for connection')" > /dev/null 2>&1
 do
     echo "Waiting for MongoDB..."
     sleep 2
 done
+
 echo "MongoDB is ready!"
