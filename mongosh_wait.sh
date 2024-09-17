b5f7153420563854cbe4fb30827d1956f5503ce1
Jesica Basile <jessbrenba@gmail.com>
2024-09-17 15:42:30 -0300

    Delete mongosh_wait.sh

--- mongosh_wait.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Waiting for MongoDB to be ready..."
-until docker exec -it mongodb mongo --eval "print(\"waited for connection\")"
-do
-    echo "Waiting for MongoDB..."
-    sleep 2
-done
-echo "MongoDB is ready!"
