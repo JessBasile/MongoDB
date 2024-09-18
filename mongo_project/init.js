859bc9cd1ad85d2ddba142355c50cd32ca8a1b93
Jesica Basile <jessbrenba@gmail.com>
2024-09-17 22:29:16 -0300

    init.js

--- mongo_project/init.js
+++ mongo_project/init.js
@@ -1,8 +1,8 @@
-use CV_Jesica_Basile
+use CV_Jesica_Basile;
 
-db.createCollection("datos_personales")
-db.createCollection("experiencia_profesional")
-db.createCollection("formacion_academica")
-db.createCollection("certificaciones")
-db.createCollection("formacion_docente")
-db.createCollection("experiencia_docente")
+db.createCollection("datos_personales");
+db.createCollection("experiencia_profesional");
+db.createCollection("formacion_academica");
+db.createCollection("certificaciones");
+db.createCollection("formacion_docente");
+db.createCollection("experiencia_docente");
